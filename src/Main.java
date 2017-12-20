1868e1cb6df2c331112dd72ea833150adfa58a1b
becca <chowr@purdue.edu>
2017-12-21 07:14:19 +0800

    Add Main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main (String[] args) throws Exception {
+        for(int i = 0; i < 4; i++) {
+            Person person = new Person();
+            System.out.println("local count: " + person.localCount + " total count: " + Person.instanceCount);
+        }
+    }
+}
